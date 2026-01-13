273b8e59e315b98ed1b4238758c289c2c3b91496
Ben Koziol <bkoziol@redlineperf.com>
2026-01-13 12:45:00 -0700

    add work

--- work.sh
+++ work.sh
@@ -11,3 +11,10 @@ git pull
 cd /glade/derecho/scratch/benkoz/sandbox/srw/benkozi/aqm-data/expt_dirs/aqm_grid_AQM_NA13km_suite_GFS_v16
 rocotostat -w FV3LAM_wflow.xml -d FV3LAM_wflow.db -v 10
 ./launch_FV3LAM_wflow.sh
+
+# compile on derecho --------------
+
+#module use <srw>/modulefiles
+#module load build_derecho_intel
+#cd <git-rocoto>
+#bash INSTALL --with-xml2-config=/glade/work/epicufsrt/contrib/derecho/libxml2-v2.12.5/xml2-config
