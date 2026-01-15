cdfbf0b8f7336ba4edc188e3016bab2c4caaced7
Ben Koziol <bkoziol@redlineperf.com>
2026-01-15 11:24:14 -0700

    adjust sleep time

--- lib/workflowmgr/workflowengine.rb
+++ lib/workflowmgr/workflowengine.rb
@@ -1394,7 +1394,8 @@ module WorkflowMgr
             # Increment unknown counter
             job.nunknowns+=1
 
-            sleep_time = 30 * (10**(job.nunknowns - 1))
+            # sleep_time = 30 * (2**(job.nunknowns - 1))
+            sleep_time = 30 * job.nunknowns
             #tdk:rm
             File.open("/glade/u/home/benkoz/htmp/qstat.out", "a") do |f|
               f.puts "sleep_time="
